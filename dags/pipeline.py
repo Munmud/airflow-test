f40706d988a694a590c99dab088288d1bd1627f4
munmud <moontasir042@gmail.com>
2025-04-25 18:07:58 +0600

    pipeline template added

---
+++ dags/pipeline.py
@@ -0,0 +1,19 @@
+def ingest_data(**kwargs):
+    # Your data ingestion code here
+    pass
+
+def preprocess_data(**kwargs):
+    # Your data preprocessing code here
+    pass
+
+def train_model(**kwargs):
+    # Your model training code here
+    pass
+
+def evaluate_model(**kwargs):
+    # Your model evaluation code here
+    pass
+
+def deploy_model(**kwargs):
+    # Your model deployment code here
+    pass
